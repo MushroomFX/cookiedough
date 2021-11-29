f9cd0dae26678cbea2c585b2c6c509361ac70704
MushroomFX <dergoelm26@gmail.com>
2021-11-29 13:00:26 +0200

    init

--- cookiedough_visulaizer.js
+++ cookiedough_visulaizer.js
@@ -1,21 +1,18 @@
 //cookie = [string,number,number]
 
-function cd_visulaiz_RL(){
-  var cd_visulaiz_interval = setInterval(function(){
-    document.getElementsByTagName('cd_render')[0].innerHTML = cd_makeTableHTML(cd_prepVis())
-  },100)
-  
-}
-
 function cd_visulaiz() {
   var cds = cd_prepVis()
   var cd_render = document.getElementsByTagName('cd_render')[0]
  if (cd_render.value == 1){
+   
+  clearInterval(cd_visulaiz_interval)
   cd_render.value = 0
   cd_render.innerHTML = ""
  } else {
   cd_render.value = 1
-  cd_render.innerHTML = cd_makeTableHTML(cds)
+  var cd_visulaiz_interval = setInterval(function(){
+    document.getElementsByTagName('cd_render')[0].innerHTML = cd_makeTableHTML(cd_prepVis())
+  },100)
 }
  return document.getElementsByTagName('cd_render')[0].value;
 }
